932a4564a92b29c1d614b764570f86ad9200ff86
Jean Georges Perrin <jperrin@lumeris.com>
2018-08-23 14:27:21 -0400

    Cleanup email

--- src/main/java/net/jgp/labs/email/lab100firstEmails/FirstMail.java
+++ src/main/java/net/jgp/labs/email/lab100firstEmails/FirstMail.java
@@ -1,37 +1,38 @@
 package net.jgp.labs.email.lab100firstEmails;
 
-import net.jgp.labs.email.lab100firstEmails.utils.DebugUtils;
 import net.jgp.labs.email.lab100firstEmails.utils.MailMessage;
 import net.jgp.labs.email.lab100firstEmails.utils.MailSender;
+import net.jgp.labs.utils.DebugUtils;
 
 public class FirstMail {
 
-	public static void main(String[] args) {
-		System.out.println("Sending e-mail");
+  public static void main(String[] args) {
+    System.out.println("Sending e-mail");
 
-		// config
-		MailSender.setSmtpServer("smtp.gmail.com");
-		MailSender.setSmtpUser("emailsender@redangus.org");
-		MailSender.setSmtpPassword("SwP12%send#1");
+    // config
+    MailSender.setSmtpServer("smtp.gmail.com");
+    MailSender.setSmtpUser("user@mailservice");
+    MailSender.setSmtpPassword("password");
 
-		// Message prep
-		MailMessage mm = null;
-		boolean emailSentSuccessfully = false;
+    // Message prep
+    MailMessage mm = null;
+    boolean emailSentSuccessfully = false;
 
-		mm = new MailMessage();
-		mm.setSubject("Your account number request for REDSPro");
-		mm.setSalutation("Dear Jean Georges,");
-		mm.setBody("Lorem Ipsum");
-		mm.addTo("jgp@jgp.net", "xyz");
-		emailSentSuccessfully = mm.send();
-		if (emailSentSuccessfully == true) {
-			System.out.println("Done");
-		} else {
-			System.out.println("Fail: " + mm.getLastError());
-		}
-		
-		DebugUtils.printClassPath();
-		DebugUtils.printSplittedClassPath();
-	}
+    mm = new MailMessage();
+    mm.setSubject(
+        "Your account number request for Smurf v1");
+    mm.setSalutation("Dear Jean Georges,");
+    mm.setBody("Lorem Ipsum");
+    mm.addTo("jgp@jgp.net", "xyz");
+    emailSentSuccessfully = mm.send();
+    if (emailSentSuccessfully == true) {
+      System.out.println("Done");
+    } else {
+      System.out.println("Fail: " + mm.getLastError());
+    }
+
+    DebugUtils.printClassPath();
+    DebugUtils.printSplittedClassPath();
+  }
 
 }
